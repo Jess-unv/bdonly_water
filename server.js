2308697253925b18c160cbba409fffe9549db011
Jess-unv <158201616+Jess-unv@users.noreply.github.com>
2024-07-22 21:01:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const pool = require('./conexion'); 
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const infoplantas = require('./routes/tabla_infoplantas');
-const infoplantas2 = require('./routes/tabla_infoplantas2');
-const plantas = require('./routes/tabla_plantas');
-const usuarios = require('./routes/tabla_usuarios');
+const infoplantas = require("./routes/tabla_infoplantas");
+const infoplantas2 = require("./routes/tabla_infoplantas2");
+const usuarios = require("./routes/tabla_usuarios");
 
 const app = express();
+
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, 
+}));
+
 app.use(bodyParser.json());
+app.use("/infoplantas", infoplantas);
+app.use("/infoplantas2", infoplantas2);
+app.use("/usuarios", usuarios);
 
-const corsOptions = {
-  origin: ['http://localhost:5173'],  
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions)); 
-
-
-app.options('*', cors(corsOptions));
-
-
-app.use('/infoplantas', infoplantas);
-app.use('/infoplantas2', infoplantas2);
-app.use('/plantas', plantas);
-app.use('/usuarios', usuarios);
-
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
 });
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
