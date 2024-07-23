0e3ce7aed19ba427dd917f11c9909da6f4e10ba3
Jess-unv <158201616+Jess-unv@users.noreply.github.com>
2024-07-22 18:16:02 -0700

    Update tabla_plantas.js

--- routes/tabla_plantas.js
+++ routes/tabla_plantas.js
@@ -3,8 +3,7 @@ const Router = express.Router();
 const mysqlConexion = require("../conexion");
 
 Router.get("/", (req, res) => {
-    
-    mysqlConexion.query("SELECT * FROM plantas", (err, rows) => {
+    mysqlConexion.query("SELECT * from plantas", (err, rows) => {
         if (err) {
             console.error("Error en la consulta:", err);
             res.status(500).send("Error en la consulta a la base de datos");
