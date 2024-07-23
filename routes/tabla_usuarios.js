aeead9d3c61b17721747bf8875f36898d7c6a679
Jess-unv <158201616+Jess-unv@users.noreply.github.com>
2024-07-22 18:12:55 -0700

    Update tabla_usuarios.js

--- routes/tabla_usuarios.js
+++ routes/tabla_usuarios.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = express.Router();
 const mysqlConexion = require("../conexion");
 
+
 Router.get("/", (req, res) => {
     mysqlConexion.query("SELECT * from usuarios", (err, rows) => {
         if (err) {
@@ -11,6 +12,27 @@ Router.get("/", (req, res) => {
             res.json(rows);
         }
     });
+});
+
+
+Router.post("/", (req, res) => {
+    const { username, password } = req.body;
+    mysqlConexion.query(
+        "SELECT * FROM usuarios WHERE email = ? AND contra = ?",
+        [username, password],
+        (err, rows) => {
+            if (err) {
+                console.error("Error en la consulta:", err);
+                res.status(500).send("Error en la consulta a la base de datos");
+            } else {
+                if (rows.length > 0) {
+                    res.json({ success: true, user: rows[0] });
+                } else {
+                    res.json({ success: false, message: "Credenciales inválidas" });
+                }
+            }
+        }
+    );
 });
 
 module.exports = Router;
